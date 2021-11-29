02a0ea26170154c8a6cd6a25e06f8e84e718f64a
mr-srivastava <aadarsh.srivastava.007@gmail.com>
2021-11-29 22:45:51 +0530

    landing page design

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import './LandingPage.scss';
+import walletImg from '../../assets/wallet.svg';
 
 const LandingPage = (props: any) => {
-  return <div>KharchaPaani</div>;
+  return (
+    <div className="wrapper">
+      <div className="background-shape" />
+      <div className="header">
+        <span>Kharcha</span>Paani
+      </div>
+      <div className="content">
+        <div className="main-content">
+          Keep your <span>Kharcha</span> Paani Sorted
+        </div>
+        <div className="description">
+          There will be no more fussing with change, misplaced receipts, or
+          arguments about the amount. Simply enter all of your spending, and
+          KharchaPaani will show you who owes what to whom and how much they
+          owe.
+        </div>
+      <button className="cta-btn">CREATE A GROUP</button>
+
+      </div>
+
+      <img src={walletImg} alt="walletImg" />
+    </div>
+  );
 };
 
 export { LandingPage };
