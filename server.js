a3caafc7dbc2132cd674692a2924831eb4893892
mr-srivastava <aadarsh.srivastava.007@gmail.com>
2022-01-21 00:21:36 +0530

    add server

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+//
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "https://kharcha-paani.herokuapp.com/",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("**Origin of request" + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin accepted");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 //Listen
 app.listen(port, () => console.log(`Listening on Port ${port}`));
+
+//serve react
+if (process.env.NODE_ENV === "production") {
+  //Serve any static file
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  //Handle react route and return all requests to react
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 // Get Route
 app.get("/api", (req, res) => {
